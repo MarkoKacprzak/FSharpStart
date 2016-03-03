62b77b3f8a61fbdb0db30a773f13db4c52e81947
Marek Kacprzak <marek.kacprzak@windowslive.com>
2016-03-03 20:04:56 +0100

    Sequence to Array from CLI

--- my/interactive.fsx
+++ my/interactive.fsx
@@ -7,3 +7,20 @@ let lastDays years=
             yield lastDay.Date
     |]
 lastDays 2014
+
+let initializeArray = Array.create 100 1.1
+
+let emptyArray: string[] = Array.zeroCreate 20
+
+let lastDay2 year =
+    Array.init 12 ( fun i ->
+        let month = i + 1
+        let firstDay = new DateTime(year,month,1)
+        let lastDay = firstDay.AddDays(float(DateTime.DaysInMonth(year,month)-1))
+        lastDay
+    )
+    
+let a=lastDay2 2011
+
+let files = System.IO.Directory.EnumerateFiles(@"c:\")
+            |> Array.ofSeq
