7de124a43949ca1fbbc10dfab0a39705c525b325
Marek Kacprzak <marek.kacprzak@windowslive.com>
2016-03-05 09:47:26 +0100

    GEOMETRY SAMPLE

--- my/array.fsx
+++ my/array.fsx
@@ -17,9 +17,22 @@ let MatchDemo() =
      | [1;_] -> printfn "That was example4"
      | [_;_] -> printfn "That was example5"
      | [4;x;9] -> printfn "That was example 6: the second element is %i" x
-     | a::t -> printfn "That was example7: the rest of the list is %A %A" a t
+     | 1::t -> printfn "That was example7: the rest of the list is %A" t
      | _ as mystery -> printfn "This was the default match: %A" mystery
 
   examples
   |> List.iter matchIt
 MatchDemo()
+
+type Geometry = 
+  | Point 
+  | Circle of double 
+  | Square of double
+  | Rectangle of double * double
+
+let PrintCircles() =
+  let items = [Point; Point; Circle(3.2); Square(5.0); Rectangle(4.5, 9.2); Circle(1.0)]
+  for Circle(r) in items do
+     printfn "Circle %f" r
+     
+PrintCircles()
