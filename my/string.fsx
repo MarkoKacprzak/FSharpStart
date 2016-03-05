8d10bee16386bd5e03978eebe771b6e083052ff9
Marek Kacprzak <marek.kacprzak@windowslive.com>
2016-03-05 15:44:34 +0100

    check if first string is number

--- my/string.fsx
+++ my/string.fsx
@@ -30,3 +30,56 @@ let spaceOut inputString =
      String.collect (fun c -> sprintf "%c " c) inputString
 printfn "%s" (spaceOut "Hello World!")
 
+// EXISTS
+
+// Signature:
+// String.exists : (char -> bool) -> string -> bool
+// Usage:
+// String.exists predicate str
+// predicate char -> bool - Funkcja testowania każdego znaku ciągu.
+// str - Ciąg wejściowy
+let containsUppercase string1 =
+    if (String.exists (fun c -> System.Char.IsUpper(c)) string1) then
+        printfn "The string \"%s\" contains uppercase characters." string1
+    else
+        printfn "The string \"%s\" does not contain uppercase characters." string1
+containsUppercase "Hello World!"
+containsUppercase "no"
+
+// FORALL
+
+// Signature:
+// String.forall : (char -> bool) -> string -> bool
+// Usage:
+// String.forall predicate string
+// prediate char -> bool Funkcja testowania każdego znaku ciągu.
+// str - Ciąg wejściowy.
+let isWholeNumber string1 =
+    if (String.forall (fun c -> System.Char.IsDigit(c)) string1) then
+        printfn "The string \"%s\" is a whole number." string1
+    else
+        printfn "The string \"%s\" is not a valid whole number." string1
+isWholeNumber "8005"
+isWholeNumber "51a231"
+
+// INIT
+
+// Signature:
+// String.init : int -> (int -> string) -> string
+// Usage:
+// String.init count initializer
+// count - Liczba ciągów zainicjować.
+// initializer  int -> string - Funkcja indeks i produkują ciąg może być łączone z innymi.
+let string1 = String.init 10 (fun i -> i.ToString())
+printfn "%s" string1
+let string2 = String.init 26 (fun i ->
+    sprintf "%c" (char (i + int 'A')))
+printfn "%s" string2
+
+open System
+let stringwq="asd"
+let first=stringwq.[0]
+let char1=char first
+let isNumer=Char.IsDigit(char1)
+printfn "First from string %s is number: %b" stringwq isNumer
+
